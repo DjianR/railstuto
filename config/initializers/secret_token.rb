3251982d5f3b1fa19678fdde454f5d48bcf294eb
DjianR <rojas.lopez.g@linuxcorp.net>
2013-09-01 02:53:15 -0400

    Inicializar repositorio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railstuto::Application.config.secret_key_base = 'e1e6fa2b18a478efb4a062715f1eb631ee3650e8a5f43ab406107b5bdbce2ea84709460cbe00e8115420645f2efa3a76c4beae839143e7f4ed84abd175becbc2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Railstuto::Application.config.session_store :cookie_store, key: '_railstuto_session'
